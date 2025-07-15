f04f47bbdf59de707e6f43baf7d9049f2905fa6c
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-07-15 12:12:24 +0545

    fix url

--- app.py
+++ app.py
@@ -694,6 +694,6 @@ def status(job_id):
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # 8080 fallback
+    app.run(host="0.0.0.0", port=port, debug=True)
